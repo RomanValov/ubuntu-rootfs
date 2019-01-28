3499306d342cba8ef36b532e68715bd085b9b256
Joshua Peek <josh@joshpeek.com>
2019-01-28 14:20:26 -0800

    Fix file perms

--- run-qemu-test.sh
+++ run-qemu-test.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+rm -f ubuntu.img ubuntu.tar
+
 docker build --tag ubuntu-rootfs .
 
 container=$(docker create ubuntu-rootfs)
@@ -9,9 +11,11 @@ docker export --output=ubuntu.tar $container
 docker rm $container
 
 sudo virt-make-fs ubuntu.tar ubuntu.img
+sudo chown $USER ubuntu.img
 rm ubuntu.tar
 
 sudo qemu-system-x86_64 \
   -kernel /boot/vmlinuz-$(uname -r) \
   -append "root=/dev/sda rw" \
-  -hda ubuntu.img
+  -drive file=ubuntu.img,format=rawls
+rm ubuntu.img
