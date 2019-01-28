b59bb6a2152760b56b7eed889c7443f9fefd59cd
Joshua Peek <josh@joshpeek.com>
2019-01-28 14:11:10 -0800

    Add qemu test script

---
+++ run-qemu-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+container=$(docker create ubuntu-rootfs)
+docker export --output=ubuntu.tar $container
+docker rm $container
+
+sudo virt-make-fs ubuntu-rootfs.tar ubuntu.img
+rm ubuntu.tar
+
+sudo qemu-system-x86_64 \
+  -kernel /boot/vmlinuz-$(uname -r) \
+  -append "root=/dev/sda rw" \
+  -hda ubuntu.img
