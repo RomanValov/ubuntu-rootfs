b6f9182a3c8ce1ac520074d3de60ff9f969c4037
Joshua Peek <josh@joshpeek.com>
2019-01-28 14:21:23 -0800

    Use qcow2

--- run-qemu-test.sh
+++ run-qemu-test.sh
@@ -10,12 +10,12 @@ container=$(docker create ubuntu-rootfs)
 docker export --output=ubuntu.tar $container
 docker rm $container
 
-sudo virt-make-fs ubuntu.tar ubuntu.img
+sudo virt-make-fs --format=qcow2 ubuntu.tar ubuntu.img
 sudo chown $USER ubuntu.img
 rm ubuntu.tar
 
 sudo qemu-system-x86_64 \
   -kernel /boot/vmlinuz-$(uname -r) \
   -append "root=/dev/sda rw" \
-  -drive file=ubuntu.img,format=raw
+  -hda ubuntu.img
 rm ubuntu.img
