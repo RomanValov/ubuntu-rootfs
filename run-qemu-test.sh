ad89426a5af587c1b8cf20f2aab589e4052e572f
Roman Valov <roman.valov@gmail.com>
2019-02-07 16:54:12 +0000

    Fix grub install to ubuntu.img.

--- run-qemu-test.sh
+++ run-qemu-test.sh
@@ -13,11 +13,12 @@ sudo virt-make-fs --partition --format=qcow2 --size=+500M ubuntu.tar ubuntu.img
 sudo chown $USER ubuntu.img
 rm ubuntu.tar
 
-sudo qemu-nbd -d /dev/nbd0
 sudo modprobe nbd max_part=16
+sudo qemu-nbd -d /dev/nbd0
 sudo qemu-nbd -c /dev/nbd0 ubuntu.img
+sleep 1
 sudo mount /dev/nbd0p1 /mnt
-sudo grub-install --root-directory=/mnt /dev/nbd0
+sudo grub-install --target=i386-pc --root-directory=/mnt /dev/nbd0
 sudo umount /mnt
 sudo qemu-nbd -d /dev/nbd0
 
